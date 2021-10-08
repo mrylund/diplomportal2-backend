48c4ecabb212e4c0ab0eaa2bbf8b4cd13af67d78
Rasmus <s152716@student.dtu.dk>
2021-10-08 16:30:24 +0200

    update schedule type

--- src/dummy.ts
+++ src/dummy.ts
@@ -1,27 +1,36 @@
-import { Course, Student } from "./interfaces";
+import { Course, Student, Day } from "./interfaces";
+export const courses: Course[] = [{
+    courseNumber: '62582',
+    title: 'Komplekse systemer og Devops',
+    // participants: students,
+    weekDay: 'ti0'
+}, {
+    courseNumber: '62598',
+    title: 'Kunstig intelligens i computerspil',
+    // participants: students,
+    weekDay: 'ti1'
+}, {
+    courseNumber: '12345',
+    title: 'Giraffer',
+    // participants: students,
+    weekDay: 'f0'
+}]
+
+export const schedule: Day[] = courses.map(course => {
+    return {
+        weekDay: course.weekDay,
+        course: course
+    }
+})
 
 export const students: Student[] = [{
     name: 'Patrick Lopdrup Hansen',
-    studyNumber: 's185092'
+    studyNumber: 's185092',
+    courses: courses,
+    schedule: schedule
 }, {
     name: 'Rasmus Strange Jakobsen',
-    studyNumber: 's152716'
+    studyNumber: 's152716',
+    courses: courses,
+    schedule: schedule
 }]
-
-export const courses: Course[] = [{
-        courseNumber: '62582',
-        title: 'Komplekse systemer og Devops',
-        participants: students,
-        weekDay: [{day: 'Tirsdag', period: 0}]
-    }, {
-        courseNumber: '62598',
-        title: 'Kunstig intelligens i computerspil',
-        participants: students,
-        weekDay: [{day: 'Tirsdag', period: 1}]
-    }, {
-        courseNumber: '12345',
-        title: 'Giraffer',
-        participants: students,
-        weekDay: [{day: 'Fredag', period: 1}]
-    }
-]
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,11 +3,19 @@
 export type Course = {
     courseNumber: string;
     title: string;
-    participants: Student[];
-    weekDay: {day: string, period: Number}[];
+    // participants: Student[];
+    weekDay: string;
 }
 
 export type Student = {
     studyNumber: string;
     name: string;
+    courses: Course[];
+    schedule: Day[];
+}
+
+
+export type Day = {
+    weekDay: string;
+    course: Course;
 }
