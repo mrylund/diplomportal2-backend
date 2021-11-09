315086bb03960b2040dcda377e8f2b33d0febb54
Patrick <patrick.lopdrup@gmail.com>
2021-11-09 12:32:48 +0100

    Debugging for prod environment

--- src/login/logIn.ts
+++ src/login/logIn.ts
@@ -2,6 +2,8 @@ import axios, { AxiosResponse } from 'axios'
 import { JWTHandler } from './jwtHandler'
 
 const backend_url = Number(process.env.BACKEND_URL) || "http://localhost:80/";
+console.log("env backend url:", process.env.BACKEND_URL)
+console.log("backend url in use:", backend_url)
 
 export class LogIn {
 
