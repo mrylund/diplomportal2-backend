5b3653590b5847378089fa4f7860e74655880dc8
Rylund <martin@rylund.eu>
2021-12-03 03:17:14 +0100

    Removed .jest

--- .jest/setEnvVars.js
+++
@@ -1,3 +0,0 @@
-process.env.DATABASE_URL="postgresql://postgres:9vfBx63B5aCEfB@130.225.170.194:5432/diplomportal_test?schema=public"
-process.env.BACKEND_URL="http://localhost:80/"
-process.env.FRONTEND_URL="http://localhost:443/"
